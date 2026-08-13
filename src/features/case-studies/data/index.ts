4f34a2acaf80f2be1a2ed73fb12c4e755f744144
Jaymin Maheta <jbmaheta@augmentedtechlabs.com>
2026-08-13 16:15:21 +0530

    feat: add Valyxto case study data

--- src/features/case-studies/data/index.ts
+++ src/features/case-studies/data/index.ts
@@ -1,8 +1,10 @@
 import type { CaseStudyData } from "../types";
 import { compito } from "./compito";
 import { farmgate } from "./farmgate";
+import { valyxto } from "./valyxto";
 
 export const caseStudies: Record<string, CaseStudyData> = {
   compito,
   farmgate,
+  valyxto,
 };
